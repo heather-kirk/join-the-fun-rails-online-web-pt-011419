f9e648b83583b427d5192dee44a6ff6afd5d63ab
Heather Kirk <hskirk@gmail.com>
2019-06-09 15:54:43 +0000

    Automatically backed up by Learn

--- db/migrate/20190609155424_add_column_to_rides.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnToRides < ActiveRecord::Migration[5.0]
-  def change
-    add_column :rides, :taxi_id, :string
-    add_column :rides, :, :integer
-    add_column :rides, :passenger_id, :string
-    add_column :rides, :, :integer
-  end
-end
